7648d4cba2490ee34d32cee2f12eea0eb3d30c33
ADM <adm@LAB-RJSE204.local>
2019-05-17 15:35:14 -0300

    ta daora

--- estr.cpp
+++ estr.cpp
@@ -5,34 +5,9 @@ using namespace std;
 
 struct trie {
   trie *children[26];
-  vector<int> links;
+  vector<int> indexes;
 };
 
-trie* init_trie () {
-  trie *tree = new trie();
-  for (int i = 0; i < 26; i++) {
-    tree->children[i] = NULL;
-  };
-  return tree;
-};
-
-void put_word (trie *tree, string word, int index) {
-  trie *branch = tree;
-  int i = 0;
-  while (true) {
-    if (word[i] == '\0') {
-      branch->links.push_back(index);
-      return;
-    };
-    if (branch->children[int(word[i])-97]) {
-     branch = branch->children[int(word[i])-97];
-     i++;
-    } else {
-      trie *news = init_trie();
-      branch->children[int(word[i])-97] = news;
-    };
-  };
-};
 
 void print(vector<int> vec) {
   for (vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i)
@@ -40,19 +15,20 @@ void print(vector<int> vec) {
   cout << "\n";
 };
 
+
 void search(trie *tree, string word) {
   trie *branch = tree;
   int i = 0;
   while (true) {
     if (word[i] == '\0') {
       cout << "links :)\n";
-      print(branch->links);
+      print(branch->indexes);
       return;
     };
-    if (branch->children[int(word[i])-97]) {
-      branch = branch->children[int(word[i])-97];
+    branch = branch->children[int(word[i])-97];
+    if (branch)
       i++;
-    } else {
+    else {
       cout << "no links :(\n";
       return;
     };
@@ -60,15 +36,42 @@ void search(trie *tree, string word) {
 };
 
 
+trie* initialize_trie () {
+  trie *tree = new trie();
+  for (int i = 0; i < 26; i++) {
+    tree->children[i] = NULL;
+  };
+  return tree;
+};
+
+
+void put_word (trie *tree, string word, int index) {
+  trie *branch = tree;
+  int i = 0;
+  while (true) {
+    if (word[i] == '\0') {
+      branch->indexes.push_back(index);
+      return;
+    };
+    if (branch->children[int(word[i])-97]) {
+     branch = branch->children[int(word[i])-97];
+     i++;
+    } else {
+      branch->children[int(word[i])-97] = initialize_trie();
+    };
+  };
+};
+
+
 int main() {
-  trie *a = init_trie();
-  put_word(a, "ab", 1);
-  put_word(a, "fuck", 2);
-  put_word(a, "oie", 3);
+  trie *tree = initialize_trie();
+  put_word(tree, "abc", 1);
+  put_word(tree, "abd", 2);
+  put_word(tree, "abe", 3);
 
   string word;
   while (true) {
     cin >> word;
-    search(a, word);
+    search(tree, word);
   };
 };
