ad25f1c129610f8992b5752507926e89ec814a84
ADM <adm@LAB-RJSE204.local>
2019-05-29 15:26:07 -0300

    printando titles

--- testing.cpp
+++ testing.cpp
@@ -5,12 +5,21 @@
 
 using namespace std;
 
-void print_vector(vector<int> *vec) {
-  if (vec) {
-    for (vector<int>::const_iterator i = vec->begin(); i != vec->end(); ++i)
-      cout << *i << ' ';
+void print_titles(vector<int> *indexes, vector<string> titles) {
+  if (indexes) {
+    char answer;
+    int k = 0;
+    for (vector<int>::const_iterator index = indexes->begin(); index != indexes->end() and answer != 'n'; ++index, k++) {
+      cout << "[" << k+1 << "] " << titles[*index] << endl;
+      if (k % 20 == 19) {
+        cout << "More results? (y, n): ";
+        cin >> answer;
+      };
+    };
+    cout << "Do you want to open any result [n or result number]? ";
+    // TO-DO
+    cin >> answer;
   };
-  cout << endl;
 };
 
 
@@ -19,25 +28,37 @@ int main() {
   vector<string> titles;
   read_and_insert(tree, titles);
 
-  string word;
-  vector<int> *indexes = new vector<int>;
+  string word = "snull";
   string snull = "snull";
+  vector<string> strings;
+
   auto start = chrono::high_resolution_clock::now();
   auto finish = chrono::high_resolution_clock::now();
   chrono::duration<double> elapsed;
 
   while (true) {
-    cout << "Word: ";
-    cin >> word;
-    search(tree, snull);
+    if (word.length() > 0)
+      cout << "\"!q\" if you want to quit or type a word to do a search: ";
 
-    start = chrono::high_resolution_clock::now();
-    indexes = search(tree, word);
-    finish = chrono::high_resolution_clock::now();
-    elapsed = finish - start;
+    getline(cin, word);
+    if (word == "!q") return 0;
 
-    cout << "... Elapsed time: " << elapsed.count() << " seconds or " << 1000*elapsed.count() << " milliseconds or " << 1000000*elapsed.count() << " microseconds\nIndex: ";
-    print_vector(indexes);
+    if (word.length() > 0) {
+      vector<vector<int>*> *indexes = new vector<vector<int>*>;
+
+      convert(word);
+      strings = word_breaker(word);
+      search(tree, snull);
+
+      start = chrono::high_resolution_clock::now();
+      for (int i = 0; i < strings.size(); i++)
+        indexes->push_back(search(tree, strings[i]));
+      finish = chrono::high_resolution_clock::now();
+      elapsed = finish - start;
+
+      cout << "About " << (*indexes)[0]->size() << " results (" << 1000000*elapsed.count() << " microseconds)\n";
+
+      print_titles((*indexes)[0], titles);
+    };
   };
-  return 0;
 };
