fc20e9ce8479a3b874b5435974252b0813153e44
ADM <adm@LAB-RJSE204.local>
2019-05-30 16:11:31 -0300

    commit

--- testing.cpp
+++ testing.cpp
@@ -4,14 +4,16 @@
 
 using namespace std;
 
+vector<vector<int> > words; vector<string> titles; vector<int> wnull; int position; int size; int i; int k; string word; string answer;
+auto inter = new vector<int> ();
 auto start = chrono::high_resolution_clock::now();
 auto finish = chrono::high_resolution_clock::now();
 chrono::duration<double> elapsed;
 
 void print_titles(const vector<int> *indexes, const vector<string> &titles) {
   if (indexes) {
-    string answer = "";
-    int k = 0;
+    answer  = "";
+    k = 0;
     for (vector<int>::const_iterator index = indexes->begin(); index != indexes->end() and answer != "n"; k++) {
       cout << "[" << k+1 << "] " << titles[*index] << endl;
       ++index;
@@ -27,14 +29,14 @@ void print_titles(const vector<int> *indexes, const vector<string> &titles) {
 };
 
 vector<int> *intersect(const vector<vector<int>*> &indexes, const int &size) {
-  auto inter = indexes[0];
-  for (int k = 1; k < size; k++) {
-    if (inter and indexes[k]) {
-      for (int i = 0; i < inter->size(); i++) {
-        if (find(indexes[k]->begin(), indexes[k]->end(), (*inter)[i]) == indexes[k]->end())
-          (*inter)[i] = -1;
-      };
-    } else return NULL;
+  if (!indexes[0]) return NULL;
+  inter = indexes[0];
+  for (i = 1; i < size; i++) {
+    if (!indexes[i]) return NULL;
+    for (k = 0; k < inter->size(); k++) {
+      if (find(indexes[i]->begin(), indexes[i]->end(), (*inter)[k]) == indexes[i]->end())
+        (*inter)[k] = -1;
+    };
   };
   if (size != 1)
     inter->erase(remove(inter->begin(), inter->end(), -1), inter->end());
@@ -43,30 +45,28 @@ vector<int> *intersect(const vector<vector<int>*> &indexes, const int &size) {
 
 
 int main() {
-  string word = "snull";
-  vector<vector<int> > strings; vector<string> titles; vector<int> snull;
-  int post; int size; vector<int> *inter = new vector<int> ();
-  for (int i = 0; i < 36; i++) snull.push_back(i);
+  for (i = 0; i < 36; i++) wnull.push_back(i);
 
   trie *tree = new trie();
   read_and_insert(tree, titles);
 
   cout << titles.size() << " titles\n";
   while (true) {
+    vector<vector<int>*> indexes;
+
     cout << "Type a word to perform a search or \"!q\" if you want to quit: ";
     getline(cin >> ws, word);
     if (word == "!q") return 0;
 
     convert(word);
-    strings = word_breaker(word);
-    vector<vector<int>*> indexes;
-    size = strings.size();
+    words = word_breaker(word);
+    size = words.size();
     indexes.reserve(size);
-    search(tree, snull);
+    search(tree, wnull);
 
     start = chrono::high_resolution_clock::now();
-    for (post = 0; post < size; post++)
-      indexes[post] = search(tree, strings[post]);
+    for (position = 0; position < size; position++)
+      indexes[position] = search(tree, words[position]);
     finish = chrono::high_resolution_clock::now();
     elapsed = finish - start;
 
