7a0690f0f5c95d8273c5bd236b5056eee5cbec66
ADM <adm@LAB-RJSE204.local>
2019-05-31 19:28:40 -0300

    commit

--- testing.cpp
+++ testing.cpp
@@ -1,25 +1,28 @@
 #include <chrono>
+#include <math.h>
 #include "gerate_trie.cpp"
 #include "word_suggestor.cpp"
-#include <math.h>
 
 using namespace std;
-vector<vector<int> > words; vector<PTI> titles; vector<int> wnull; int position, williams_size, i, k, last_index; string word; string answer;
+vector<vector<int> > words; vector<PTI> titles; vector<int> wnull; string word, answer; int position, size, i, k, last_index;
 auto inter = new vector<int> ();
 auto start = chrono::high_resolution_clock::now();
 auto finish = chrono::high_resolution_clock::now();
 chrono::duration<double> elapsed;
 
+
 void print_titles(const vector<int> *indexes, const vector<PTI> &titles) {
   if (indexes) {
     answer  = "";
     k = 0;
-	last_index = indexes->back();
-	vector<PTI> searched_titles;
-	for (vector<int>::const_iterator it = indexes->begin(); it != indexes->end(); it++) {
+    //
+    last_index = indexes->back();
+    vector<PTI> searched_titles;
+    for (vector<int>::const_iterator it = indexes->begin(); it != indexes->end(); it++) {
 			searched_titles.push_back(titles[*it]);
-	}
-	quickSort(searched_titles, 0, searched_titles.size());
+    }
+    quickSort(searched_titles, 0, searched_titles.size());
+    //
     for (vector<PTI>::const_iterator index = searched_titles.begin(); index != searched_titles.end() and answer != "n"; k++) {
       cout << "[" << k+1 << "] " << index->titulo << endl;
       ++index;
@@ -34,20 +37,24 @@ void print_titles(const vector<int> *indexes, const vector<PTI> &titles) {
   };
 };
 
+
 vector<int> *intersect(const vector<vector<int>*> &indexes, const int &size) {
   if (!indexes[0]) return NULL;
   inter = indexes[0];
   for (i = 1; i < size; i++) {
     if (!indexes[i]) return NULL;
     for (k = 0; k < inter->size(); k++) {
-      if (find(indexes[i]->begin(), indexes[i]->end(), (*inter)[k]) == indexes[i]->end())
+      if (find(indexes[i]->begin(), indexes[i]->end(), (*inter)[k]) == indexes[i]->end()) {
         (*inter)[k] = -1;
+        k = inter->size() - 1;
+      };
     };
   };
   if (size != 1)
     inter->erase(remove(inter->begin(), inter->end(), -1), inter->end());
   return inter;
 };
+
 
 int main() {
   for (i = 0; i < 36; i++) wnull.push_back(i);
@@ -65,19 +72,18 @@ int main() {
 
     convert(word);
     words = word_breaker(word);
-	williams_size = words.size();
-    indexes.reserve(williams_size);
+    size = words.size();
+    indexes.reserve(size);
     search(tree, wnull);
 
     start = chrono::high_resolution_clock::now();
-    for (position = 0; position < williams_size; position++)
+    for (position = 0; position < size; position++)
       indexes[position] = search(tree, words[position]);
     finish = chrono::high_resolution_clock::now();
     elapsed = finish - start;
 
-    inter = intersect(indexes, williams_size);
+    inter = intersect(indexes, size);
     cout << "About " << (inter ? inter->size() : 0) << " results (" << 1000000*elapsed.count() << " microseconds)\n";
     print_titles(inter, titles);
   };
-
 };
