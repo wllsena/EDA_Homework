61d465d74826eb5aa865cdc924a9cd846e92d3e1
ADM <adm@LAB-RJSE204.local>
2019-06-03 19:52:08 -0300

    preparando final

---
+++ generators.cpp
@@ -0,0 +1,33 @@
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/iostreams/stream.hpp>
+#include "trie.cpp"
+
+using namespace std;
+namespace bio = boost::iostreams;
+
+bio::mapped_file_params params;
+
+int main () {
+  params.path          = "trie_structure";
+  params.new_file_size = number_of_tries*sizeof(disk_trie);
+  params.flags         = bio::mapped_file::mapmode::readwrite;
+  bio::mapped_file_sink Trie_structure(params);
+
+  params.path          = "counters";
+  params.new_file_size = number_of_tries*sizeof(int);
+  params.flags         = bio::mapped_file::mapmode::readwrite;
+  bio::mapped_file_sink Counters(params);
+
+  params.path          = "indexes";
+  params.new_file_size = number_of_indexes*sizeof(int);
+  params.flags         = bio::mapped_file::mapmode::readwrite;
+  bio::mapped_file_sink Indexes(params);
+
+  disk_trie * disk_tree = (disk_trie *)Trie_structure.data();
+  int * counters = (int *)Counters.data();
+  int * indexes = (int *)Indexes.data();
+
+  gerate_trie_with_couters();
+  put_indexes();
+  gerate_txt();
+};
--- trie.cpp
+++ trie.cpp
@@ -1,35 +1,72 @@
-#include <iostream>
 #include <vector>
 
 using namespace std;
 
+struct disk_trie {
+  int children[36];
+  int kinship[2];
+};
+
 struct trie {
   trie *children[36];
-  vector<int> *indexes = NULL;
+  int index;
 };
 
+int disk_branch, position, counter;
+int trie_position = 1;
+int number_of_tries = 8233674;
+int number_of_indexes = 0 + number_of_tries; // mudar
+unsigned int global_counter = 0; // remover
 trie *branch;
-int post;
 
-vector<int> *search(trie *tree, const vector<int> &word) {
-  branch = tree;
-  for (post = 0; post != word.size(); post++) {
-    branch = branch->children[word[post]];
-    if (!branch)
-      return NULL;
+void put_word_and_counter (disk_trie *tree, int *counters, const vector<int> &word) {
+  disk_branch = 0;
+  for (position = 0; position != word.size(); position++) {
+    if (!tree[disk_branch].children[word[position]]) {
+      tree[disk_branch].children[word[position]] = trie_position;
+      tree[trie_position].kinship[0] = disk_branch;
+      tree[trie_position].kinship[1] = word[position];
+      trie_position++;
+    };
+    disk_branch = tree[disk_branch].children[word[position]];
   };
-  return branch->indexes;
+  counters[disk_branch]++;
+  global_counter++; // remover
 };
 
-void put_word (trie *tree, const vector<int> &word, const int &index) {
-  branch = tree;
-  for (post = 0; branch and post < word.size(); post++) {
-    if (!branch->children[word[post]])
-      branch->children[word[post]] = new trie();
-    branch = branch->children[word[post]];
+int disk_search(const disk_trie *tree, const vector<int> &word) {
+  disk_branch = 0;
+  for (position = 0; position != word.size(); position++) {
+    disk_branch = tree[disk_branch].children[word[position]];
+    if (!disk_branch)
+      return 0;
   };
-  if (!branch->indexes)
-    branch->indexes = new vector<int> ();
-  if (find(branch->indexes->begin(), branch->indexes->end(), index) == branch->indexes->end())
-    branch->indexes->push_back(index);
+  return disk_branch;
+};
+
+void put_index (int *indexes, const disk_trie *tree, const int *counters, const vector<int> &word, const int &index) {
+  counter = counters[disk_search(tree, word)];
+  indexes[counter]++;
+  indexes[counter + indexes[counter]] = index;
+};
+
+int search(trie *tree, const vector<int> &word) {
+  branch = tree;
+  for (position = 0; position != word.size(); position++) {
+    branch = branch->children[word[position]];
+    if (!branch)
+      return 0;
+  };
+  return branch->index;
+};
+
+trie *load_trie (const disk_trie *disk_tree) {
+  trie *pointers[number_of_tries];
+  pointers[0] = new trie();
+  for (position = 1; position < number_of_tries; position++) {
+    pointers[position] = new trie();
+    pointers[position]->index = position;
+    pointers[disk_tree[position].kinship[0]]->children[disk_tree[position].kinship[1]] = pointers[position];
+  };
+  return pointers[0];
 };
